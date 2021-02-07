c50249129c7c60022c8311bedc9a9d631f8ca59b
neopetz <61999023+neopetz@users.noreply.github.com>
2021-02-07 21:32:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jose/testing/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.jose.testing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this,MainActivity.class));
+            }
+        });
+    }
+
+
+}
